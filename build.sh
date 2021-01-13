211e0fcdd5dace722daa91e9e824b5cfb0d31f9e
estellearrc <earricau@ensc.fr>
2021-01-13 22:37:45 +0100

    modify build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+#Run this in terminal
+#+ Command to compile c++ program. here i used common one
+#Use: chmod u+x build.sh to compile
+mkdir build
+cd build
+cmake ..
+make
 
-g++ Cell.cpp Path.cpp vibes.cpp main.cpp -o maze
+./create_raster ../doc/Guerledan_Feb19_50cm_wgs84.txt 800
+
+cd images
+#exit 0
